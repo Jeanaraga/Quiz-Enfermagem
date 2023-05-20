d1c6fe3423eebc543f0d42ae8eab667fce329ab9
Jean Pedro <jeanjesuspedrobook@gmail.com>
2023-05-19 21:12:06 -0300

    main

--- script.js
+++ script.js
@@ -1,104 +1,93 @@
-// Dados iniciais
+// ;Initial Data
 let currentQuestion = 0;
 let correctAnswer = 0;
 let valorNome = "";
+// ...
 
-// Verifica se o quiz já foi respondido anteriormente
-const quizAttempted = localStorage.getItem("quizAttempted");
+// Obtém o valor atual do contador do Local Storage, se existir
+let contadorFim = parseInt(localStorage.getItem('contadorFim')) || 0;
 
-if (quizAttempted) {
-  // Se o quiz já foi respondido, exibe uma mensagem adequada
-  document.querySelector('.inicio').style.display = 'none';
-  document.querySelector('.scoreArea').style.display = 'block';
-  document.querySelector('.scoreText1').innerHTML = 'Você já tentou fazer o quiz antes.';
-  document.querySelector('.scorePct').style.color = '#000';
-  document.querySelector('.scorePct').innerHTML = '';
-  document.querySelector('.scoreText2').innerHTML = '';
-} else {
-  // Eventos
-  document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+// showQuestion();
 
-  // Função para exibir a pergunta atual
-  function showQuestion() {
-    valorNome = document.querySelector('#nome').value;
+// Events
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
 
-    if (questions[currentQuestion]) {
-      // Exibe a pergunta e as opções de resposta
-      document.querySelector('.inicio').style.display = 'none';
-      document.querySelector('.scoreArea').style.display = 'none';
-      let q = questions[currentQuestion];
+// Function
+function showQuestion() {
+     valorNome = document.querySelector('#nome').value;
 
-      let pct = (currentQuestion / questions.length) * 100;
-      document.querySelector('.progress--bar').style.width = pct + '%';
+    if(questions[currentQuestion]) {
+        document.querySelector('.inicio').style.display = 'none';
+        document.querySelector('.inicio').style.display = 'none';
+        let q = questions[currentQuestion];
 
-      document.querySelector('.questionArea').style.display = 'flex';
-      document.querySelector('.question').innerHTML = q.question;
-      document.querySelector('.options').innerHTML = '';
+        let pct = (currentQuestion / questions.length) * 100;
+        document.querySelector('.progress--bar').style.width = pct + '%';
 
-      let optionsHtml = '';
-      for (let i in q.options) {
-        optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i) + 1}</span> ${q.options[i]}</div>`;
-      }
-      document.querySelector('.options').innerHTML = optionsHtml;
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'flex';
 
-      document.querySelectorAll('.options .option').forEach(item => {
-        item.addEventListener('click', optionClickEvent);
-      });
+        document.querySelector('.question').innerHTML = q.question;
+        document.querySelector('.options').innerHTML = '';
+
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        })
+
     } else {
-      finishQuiz();
+        finishQuiz()
     }
-  }
+}
 
-  // Função para tratar o clique em uma opção de resposta
-  function optionClickEvent(e) {
+function optionClickEvent(e) {
     let clickOption = parseInt(e.target.getAttribute('data-op'));
 
-    if (questions[currentQuestion].answer === clickOption) {
-      correctAnswer++;
+    if(questions[currentQuestion].answer === clickOption) {
+        correctAnswer ++
     }
 
     currentQuestion += 1;
-    showQuestion();
-  }
+    showQuestion()
+}
 
-  // Função para finalizar o quiz
-  function finishQuiz() {
+function finishQuiz() {
     let points = (correctAnswer / questions.length) * 100;
 
-    if (points <= 30) {
-      document.querySelector('.scoreText1').innerHTML = `Tá ruim em ${valorNome} 😕`;
-      document.querySelector('.scorePct').style.color = "#ff0000";
-    } else if (points > 30 && points <= 70) {
-      document.querySelector('.scoreText1').innerHTML = `Bom ${valorNome} 🙂`;
-      document.querySelector('.scorePct').style.color = "yellow";
+    if(points <= 30) {
+        document.querySelector('.scoreText1').innerHTML = `Tá ruim em ${valorNome}😕`;
+        document.querySelector('.scorePct').style.color = "#ff0000";
+    } else if(points > 30 && points <= 70) {
+        document.querySelector('.scoreText1').innerHTML = `Bom ${valorNome}🙂`;
+        document.querySelector('.scorePct').style.color = "yellow";
     } else {
-      document.querySelector('.scoreText1').innerHTML = `Muito bom! Parabéns ${valorNome} 🙂🏆`;
-      document.querySelector('.scorePct').style.color = "green";
+        document.querySelector('.scoreText1').innerHTML = `Muito Bom, Parabéns ${valorNome}🙂🏆`;
+        document.querySelector('.scorePct').style.color = "green";
     }
 
     document.querySelector('.scorePct').innerHTML = `Acertou ${Math.floor(points)}%`;
     document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswer}`;
 
+    contadorFim++
+
+    localStorage.setItem('contadorFim', contadorFim.toString());
+    document.getElementById('quizCount').innerHTML = `Quiz respondido pela ${contadorFim}º vez`;
+
+
     document.querySelector('.scoreArea').style.display = 'block';
     document.querySelector('.questionArea').style.display = 'none';
     document.querySelector('.progress--bar').style.width = '100%';
+}
 
-    // Marca o quiz como respondido
-    localStorage.setItem('quizAttempted', 'true');
-  }
-
-  // Função para reiniciar o quiz
-  function resetEvent() {
+function resetEvent() {
     currentQuestion = 0;
     correctAnswer = 0;
     document.querySelector('.scoreArea').style.display = 'none';
     document.querySelector('.questionArea').style.display = 'none';
     document.querySelector('.inicio').style.display = 'flex';
-
-    // Remove a marcação de tentativa do quiz
-    localStorage.removeItem('quizAttempted');
-  }
-
-  // Inicia o quiz
-  showQuestion();
 }
